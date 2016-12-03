1e31bab5500ffa471aa0b04100963f1fe55b30b3
Kyle <kgifaldi@nd.edu>
2016-12-03 18:07:44 -0500

    small change

--- parser.py
+++ parser.py
@@ -7,8 +7,6 @@ from bs4 import BeautifulSoup
 def filterText(text):
     if text.parent.name in ['style', 'script', '[document]', 'head', 'title']:
         return False
-    if text in ['\n', ' ', '\r', '\t']:
-        return False
     return True
 
 #prints out url with all text from url on one line
@@ -18,7 +16,6 @@ def textParser(url):
     #format html and only print text from webpage:
     soup = BeautifulSoup(webPage.content, "lxml")
     allText = soup.findAll(text=True)
-    #print (allText[432])
     for i in allText:
         if filterText(i):
             print (i.replace('\n',' '), end='')
