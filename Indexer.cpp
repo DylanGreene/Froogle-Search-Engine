b63f9e180c495d1aaeedbd4a1dc79cd46182a447
John Nolan <jnolan5@nd.edu>
2016-12-03 18:06:06 -0500

    Switched to a heap.

--- Indexer.cpp
+++ Indexer.cpp
@@ -1,54 +1,72 @@
 #include <iostream>
 #include <sstream>
 #include <map>
-#include <queue>
+//#include <queue>
+#include <vector>
+#include <algorithm>
 #include <string>
 
 using namespace std;
 
 struct index_sort {
  bool operator()(pair<string, unsigned int> const & a, pair<string, unsigned int> const & b) {
-	return a.second > b.second; // sort by frequencies
+	 return a.second > b.second; // sort by frequencies
  }
 };
 
-typedef priority_queue<pair<string, unsigned int>, vector< pair<string, unsigned int> >, index_sort> URLpq;
+typedef vector< pair<string, unsigned int> > URLheap;
 
 int main(int argc, char *argv[]) {
-
+	
  string url_line;
  string url;
  string word;
  istringstream ss;
- map<string, URLpq > indexer;
+ map<string, URLheap > indexer;
  map<string, unsigned int> frequency;
  
  while (!cin.eof()) {
-	
-	// get url followed by all the words
-	getline(cin, url_line);
-	
-	ss.str(url_line);
-	
-	ss >> url;
-	
-	URLpq pq;
-	indexer[url] = pq;
-	while (ss >> word) {
-	 if (frequency.find(word) == frequency.end()) {
-		frequency[word] = 1;
-	 } else {
-		frequency[word] += 1;
+	 
+	 // get url followed by all the words
+	 getline(cin, url_line);
+	 
+	 ss.str(url_line);
+	 
+	 ss >> url;
+	 
+	 URLheap heap;
+	 indexer[url] = heap;
+	 
+	 while (ss >> word) {
+		 // count the frequency of each word
+		 if (frequency.find(word) == frequency.end()) {
+			 frequency[word] = 1;
+		 } else {
+			 frequency[word] += 1;
+		 }
 	 }
-	}
-	
-	for (auto it = frequency.begin(); it != frequency.end(); it++) {
-	 indexer[url].push(*it);
-	}
-	
-	
+	 
+	 // add those frequency pairs to the url indexer
+	 for (auto it = frequency.begin(); it != frequency.end(); it++) {
+		 indexer[url].push_back(*it);
+		 push_heap(indexer[url].begin(), indexer[url].end(), index_sort());
+	 }
+	 
  }
-	return 0;
+ 
+ //Test prints
+ for (auto it = indexer.begin(); it != indexer.end(); it++) {
+	 
+	 cout << it->first << endl;
+	 
+	 for (auto jt = it->second.begin(); jt != it->second.end(); jt++) {
+		 cout << jt->first << ": " << jt->second << "   ";
+	 }
+	 
+	 cout << endl;
+ }
+ 
+ return 0;
 }
 
 //make_map() { }
