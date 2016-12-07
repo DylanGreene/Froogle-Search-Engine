4edb9daf13f0b7b6912a4bb3e4f958e7373a86e6
Dylan Greene <dylantgreene@outlook.com>
2016-12-07 15:25:16 -0500

    Display top 10 results

--- src/resultsServer.cpp
+++ src/resultsServer.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <cmath>
 #include <queue>
 #include "Indexer.h"
@@ -10,26 +10,21 @@
 
 using namespace std;
 
-int calcSearchTermWeight(unsigned int val)
-{
+int calcSearchTermWeight(unsigned int val){
     int toReturn = 0;
-    for(int i = 1; i <= val; i++)
-    {
+    for(int i = 1; i <= val; i++){
         toReturn += 100/pow(2, (i-1));
     }
     return toReturn;
 }
 
-struct compareFunc
-{
-    bool operator()(const pair<string, int>& p1, const pair<string, int>& p2)
-    {
+struct compareFunc{
+    bool operator()(const pair<string, int>& p1, const pair<string, int>& p2){
         return p1.second < p2.second;
     }
 };
 
-int main(void)
-{
+int main(void){
     string searchTerm;
     Indexer ind;
     ifstream searchTerms;
@@ -39,12 +34,11 @@ int main(void)
         searchList.push_back(searchTerm);
     }
     ind.initializeIndex();
-    map<string, int> urlRanking;
-    map<string, URLheap > index = ind.getIndexer();
+    unordered_map<string, int> urlRanking;
+    unordered_map<string, URLheap > index = ind.getIndexer();
 
 
-    for(int i = 0; i < searchList.size(); i++)
-    {
+    for(int i = 0; i < searchList.size(); i++){
         auto vec = index[searchList[i]];
         for(int j = 0; j < vec.size(); j++){
             int weight = calcSearchTermWeight(vec[j].second);
@@ -53,25 +47,28 @@ int main(void)
     }
 
     string url;
-    while(cin >> url)
-    {
+    while(cin >> url){
         int numEdges;
         cin >> numEdges;
         auto it = urlRanking.find(url);
-        if(it != urlRanking.end())
-        {
+        if(it != urlRanking.end()){
             it->second += 5* numEdges;
         }
     }
     priority_queue< pair<string, int>, vector< pair<string, int> >, compareFunc> finalRank;
-    for(auto it = urlRanking.begin(); it != urlRanking.end(); it++)
-    {
+    for(auto it = urlRanking.begin(); it != urlRanking.end(); it++){
         finalRank.push(*it);
     }
-    while(!finalRank.empty())
-    {
+    for(int i = 0; i < 10; i++){
         auto it = finalRank.top();
         finalRank.pop();
         cout << it.first << endl;
     }
+    /*
+    while(!finalRank.empty()){
+        auto it = finalRank.top();
+        finalRank.pop();
+        cout << it.first << endl;
+    }
+    */
 }
