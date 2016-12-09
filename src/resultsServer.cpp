00777c1924688e19d7821091a996821c8a144912
Charles Simms Osborne <cosborn4@student00.cse.nd.edu>
2016-12-09 00:11:34 -0500

    comments

--- src/resultsServer.cpp
+++ src/resultsServer.cpp
@@ -8,9 +8,11 @@
 #include <fstream>
 #include <sstream>
 
-
+//ResultsServer class takes results of web crawl and combines them with search terms to calculate 10 best URLs
 using namespace std;
 
+//Calculates the weight of each search term
+//Weights are diminishing as url frequencies increase
 int calcSearchTermWeight(unsigned int val){
     int toReturn = 0;
     for(unsigned i = 1; i <= val; i++){
@@ -19,6 +21,7 @@ int calcSearchTermWeight(unsigned int val){
     return toReturn;
 }
 
+//Sorts priority queue based on highest ranking
 struct compareFunc{
     bool operator()(const pair<string, int>& p1, const pair<string, int>& p2){
         return p1.second < p2.second;
@@ -28,6 +31,7 @@ struct compareFunc{
 int main(void){
     string searchTerm;
 	ifstream checkIfGood("mapFile.txt");
+	//Updates the web crawler's results only if the web crawler has been run recently
 	if(!checkIfGood.good())
 	{
     	Indexer ind;
@@ -38,6 +42,7 @@ int main(void){
     ifstream searchTerms;
     searchTerms.open(".searchTerms.txt");
     vector<string> searchList;
+	//Gets the search terms from a file 
     while(searchTerms >> searchTerm){
         searchList.push_back(searchTerm);
     }
@@ -45,6 +50,7 @@ int main(void){
     unordered_map<string, int> urlRanking;
 	unordered_map<string, int> urlFrequencies;
 	ifstream checkUrlCounts(".urlCounts.txt");
+	//Gets the url frequencies
 	if(checkUrlCounts.good())
 	{
 		string url;
@@ -61,6 +67,7 @@ int main(void){
 	string line;
 	string word;
 	mapInFile.open("mapFile.txt");
+	//Gets the map that maps a string to its urls and the frequency it appears in each url
 	while(getline(mapInFile, line))
 	{
 		stringstream ss;
@@ -77,6 +84,7 @@ int main(void){
 		index[word] = urlHeap;
 	}
 
+	//Calculates the ranking of the urls based on the weight and the frequencies
     for(int i = 0; (size_t)i < searchList.size(); i++){
         auto vec = index[searchList[i]];
         for(int j = 0; (size_t)j < vec.size(); j++){
@@ -85,10 +93,12 @@ int main(void){
         }
     }
 
+	//Pushes the ranks into a priority queue to sort them
     priority_queue< pair<string, int>, vector< pair<string, int> >, compareFunc> finalRank;
     for(auto it = urlRanking.begin(); it != urlRanking.end(); it++){
         finalRank.push(*it);
     }
+	//Prints the top 10 ranks
     for(int i = 0; i < 10; i++){
         auto it = finalRank.top();
         finalRank.pop();
