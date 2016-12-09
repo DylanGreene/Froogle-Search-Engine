cd79ad66ca212d82f2fe10d8a67fb8bc67df69b5
Dylan Greene <dylantgreene@outlook.com>
2016-12-09 05:28:18 -0500

    Benchmarking

--- src/benchmark.sh
+++ src/benchmark.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-#benchmark
+# benchmark
 # iterate through depths of 1, 2, and 3
 # then run crawler with and without threading
-for i in `seq 1 2 3`;
-do
+for i in `seq 3`; do
     echo "Depth: " $i
-    echo "with threading..." #> benchMark.md
-    ./measure ./crawler.py -n $i -t -f 
+    echo "with threading..."
+    timeout 1h ./crawler.py -n $i -t -f -b
     echo "without threading..."
-    ./measure ./crawler.py -n $i -t -p
+    timeout 1h ./crawler.py -n $i -t -f -p -b
     echo "resultsServer..." $i
-    for j in `seq 1 2 3`; do
-        ./measure ./resultsServer | grep -v "http"
-    done
+    ./measure ./resultsServer 2>&1 | tail -n 1
 done
-  
-
